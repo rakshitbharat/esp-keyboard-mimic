6a2ece2eace6ad43b511ee7cb4e753941a0eda3b
GitHub <rakshitbharat@gmail.com>
2025-04-11 02:02:36 +0530

    -

--- app/src/components/ui/card.tsx
+++ app/src/components/ui/card.tsx
@@ -16,4 +16,24 @@ const Card = React.forwardRef<
 ));
 Card.displayName = "Card";
 
-export { Card };
+const CardHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    {...props}
+  />
+));
+CardHeader.displayName = "CardHeader";
+
+const CardContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+));
+CardContent.displayName = "CardContent";
+
+export { Card, CardHeader, CardContent };
